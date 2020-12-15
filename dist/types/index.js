f3fda748a22fb0059d98c93471e4e64c83c30b81
Ken Snyder <ken@ken.net>
2020-12-15 06:46:54 -0800

    transpiled

--- dist/types/index.js
+++ dist/types/index.js
@@ -1,11 +1,11 @@
 import { wait } from "common-types";
 const cache = {};
 const BASE_URL = "https://maps.googleapis.com/maps/api/js";
-export async function loadNow(library, apiKey) {
+export async function loadNow(library, apiKey, region, language) {
     if (checkIfScriptTagExists(library, apiKey)) {
         return window.google;
     }
-    await addScriptTagToBrowser(library, apiKey);
+    await addScriptTagToBrowser(library, apiKey, region, language);
     return window.google;
 }
 export async function preload(library, apiKey, delay = 2000) {
@@ -13,7 +13,7 @@ export async function preload(library, apiKey, delay = 2000) {
     await wait(delay);
     await addScriptTagToBrowser(library, apiKey);
 }
-async function addScriptTagToBrowser(library, apiKey, options = {}) {
+async function addScriptTagToBrowser(library, apiKey, region, language, options = {}) {
     if (checkIfScriptTagExists(library, apiKey)) {
         console.info(`Attempt to add script tag for the "${library}" library in Google Maps ignored as this tag already exists in the DOM${apiKey ? " [ " + apiKey + "]" : ""}`);
         return;
@@ -25,7 +25,7 @@ async function addScriptTagToBrowser(library, apiKey, options = {}) {
     const waitForLoad = () => {
         var script = document.createElement("script");
         script.id = `google-maps-${library}-js`;
-        script.src = getUrl(library, apiKey);
+        script.src = getUrl(library, apiKey, true, region, language);
         document.querySelector("head").appendChild(script);
         return new Promise(resolve => {
             window[`${library}LoaderCallback`] = () => {
@@ -43,10 +43,16 @@ function addPreloadLinkToBrowser(library, apiKey) {
     link.href = getUrl(library, apiKey, false);
     document.querySelector("head").appendChild(link);
 }
-function getUrl(library, apiKey, callback = true) {
+function getUrl(library, apiKey, callback = true, region, language) {
     let url = `${BASE_URL}?libraries=${library}&sensors=false`;
     if (apiKey) {
         url = `${url}&key=${apiKey}`;
+    }
+    if (region) {
+        url = `${url}&region=${region}`;
+    }
+    if (language) {
+        url = `${url}&language=${language}`;
     }
     if (callback) {
         url = `${url}&callback=${library}LoaderCallback`;
